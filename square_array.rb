b69ba8c435edc3eaa01c9891c69377c126ad35d9
Brian Vehige <brianvehige@gmail.com>
2020-02-16 23:14:29 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,15 +1,12 @@
-def square_array(array)
-  array.collect {|x| x**2}
-end
 
-  
-
-
-
-def square_array2(numbers)
+def square_array(numbers)
   new_array = []
   numbers.each do |squared|
     new_array << squared**2
 end
   return new_array 
+end
+
+def square_array2(array)
+  array.collect {|x| x**2}
 end
